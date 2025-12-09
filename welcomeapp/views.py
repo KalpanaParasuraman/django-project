d61ff3684487d16283e8e37ea6498405bed4237b
Kalpana <kalpana.p@christuniversity.com>
2025-12-10 00:37:54 +0530

    first commit revised

--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("This is the revised message!")
